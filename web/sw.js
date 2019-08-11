fe18002fb270eb6ceb9c5b72aec4591c477cef40
Jack Hullis <greendra8@hotmail.co.uk>
2019-08-11 15:45:34 +0100

    service worker

---
+++ web/sw.js
@@ -0,0 +1,37 @@
+importScripts('/cache-pollyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('mceguide-offline').then(function(cache) {
+     return cache.addAll([
+         '/',
+         '/css/style.css',
+         '/index.html',
+         '/img/crafting-table.png',
+         '/img/door.png',
+         '/img/muddy-pig.png',
+         '/img/backpack.png',
+         '/img/chest.png',
+         '/img/item-types.png',
+         '/img/build-plate.png',
+         '/img/friends.png',
+         '/img/beacon.png',
+         '/img/diamond_pickaxe.png',
+         '/img/clock.png',
+         '/css/img/bg_main2.png',
+         '/css/img/bg_bedrock.png',
+         'https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap'
+     ]);
+   })
+ );
+});
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
