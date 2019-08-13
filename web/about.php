e260a32ec9d7a1a66a4d7b265eec387d62180706
Jack Hullis <greendra8@hotmail.co.uk>
2019-08-13 14:07:46 +0100

    Update about.php

--- web/about.php
+++ web/about.php
@@ -13,7 +13,7 @@
             </div>
             <h3 class="list-item-title">Legal and Contact</h3>
             <div class="list-item-description">
-            <p>The Minecraft Earth Guide App <b>is not affiliated or associated with Minecraft, Mojang or Microsoft, or any official body in connection with the Minecraft Earth project.</b></p>
+            <p>The Minecraft Earth Guide App <b>is not affiliated or associated with Minecraft, Mojang or Microsoft, or any official body in connection with the Minecraft Earth game.</b></p>
             <p>All images hosted on this site are under the Creative Commons license.</p>
             <p><b>This website is affiliated with PlayMinecraftEarth.com</b>, a third party news website for the mobile game Minecraft Earth.</p>
             <p>If you need to contact the webmaster of this site over any issues, either regarding content hosted here or just general inquiries, then please send an email to app@playminecraftearth.com</p>
