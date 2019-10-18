6d2113624bb63c6cbb2fe0d26faef11efe2e31bd
Jack Hullis <greendra8@hotmail.co.uk>
2019-10-18 17:41:16 +0100

    .

---
+++ web/test.php
@@ -0,0 +1,51 @@
+<html> 
+    <link rel="stylesheet" href="../css/test.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+<header>GUIDE FOR Minecraft Earth</header>
+<header class="hidden">GUIDE FOR Minecraft Earth</header>
+
+<div class="wrapper">
+   <h1>featured articles:</h1>
+        <div class="featured-article left">
+            <img class="featured-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+         <div class="featured-article right">
+            <img class="featured-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+        <div class="clearfix"></div>
+        <div style="display:none">
+    <h1>popular guides</h1>
+        <div class="wide-article">
+            <img class="wide-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+        <div class="wide-article">
+            <img class="wide-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+        
+    <h1>beginneres guides</h1>
+        <div class="small-article">
+            <img class="small-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+        <div class="small-article">
+            <img class="small-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+        <div class="small-article">
+            <img class="small-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+        <div class="small-article">
+            <img class="small-image" src="img/minecon.png">
+            <h3>Introduction to Tappables</h3>
+        </div>
+    </div>
+</div>
+</body>
+<html> 
